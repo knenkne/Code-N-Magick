a128eef442df72f3d674b9a4af865cfdd0d03892
AlexanderBrez <jztenk@gmail.com>
2018-12-08 00:09:42 +0300

    Modules

--- js/dialog.js
+++ js/dialog.js
@@ -1,55 +1,58 @@
 'use strict';
 
-var setup = document.querySelector('.setup');
-var upload = setup.querySelector('.upload');
+(function () {
+  var setup = document.querySelector('.setup');
+  var upload = setup.querySelector('.upload');
 
-upload.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  var dragged = false;
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+  upload.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
+    var dragged = false;
 
-    var newCoords = {
-      x: setup.offsetLeft - shift.x,
-      y: setup.offsetTop - shift.y
-    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
 
-    setup.style.top = newCoords.y + 'px';
-    setup.style.left = newCoords.x + 'px';
-  };
-
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-
-    if (dragged) {
-      var onClickPreventDefault = function (preventEvt) {
-        preventEvt.preventDefault();
-        upload.removeEventListener('click', onClickPreventDefault);
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
       };
-      upload.addEventListener('click', onClickPreventDefault);
-    }
-  };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var newCoords = {
+        x: setup.offsetLeft - shift.x,
+        y: setup.offsetTop - shift.y
+      };
+
+      setup.style.top = newCoords.y + 'px';
+      setup.style.left = newCoords.x + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (preventEvt) {
+          preventEvt.preventDefault();
+          upload.removeEventListener('click', onClickPreventDefault);
+        };
+        upload.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+})();
